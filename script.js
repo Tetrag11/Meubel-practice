57b2f8dbf147f9660635f4e10a2273a0fad158cc
Tetrag11 <tehanekanayake@gmail.com>
2021-11-14 11:12:47 +0530

    firsm

---
+++ script.js
@@ -0,0 +1,10 @@
+const x = document.querySelector('#navbar_mobile');
+
+
+function show(){
+    x.style.width = '100%';
+}
+
+function hide(){
+    x.style.width = '0%';
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontSize:{
+      'xs': '0.55rem' 
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
